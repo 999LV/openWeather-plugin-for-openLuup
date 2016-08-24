5f40f59f3bc6c0e75f2d8b3b23bdfb1c2063c9c9
999LV <lv999@online.fr>
2016-08-24 13:00:31 +0200

    Version 1.1
    Major rewrite of code - cleaner and faster

--- L_openWeather.lua
+++ L_openWeather.lua
@@ -1,13 +1,12 @@
 _NAME = "openWeather"
-_VERSION = "2016.07.15"
+_VERSION = "1.1"
 _DESCRIPTION = "WU plugin for openLuup!!"
 _AUTHOR = "logread (aka LV999)"
 
 --[[
 
-		Version 1.0 (first production version)
-		changelog:	- code optimization (w/ recursive parsing of WU data table)
-					- installation via the AltUI/openLuup App Store
+Version 1.0 2016-07-15 - first production version, installation via the AltUI/openLuup App Store
+Version 1.1 2016-08-24 - major rewrite for cleaner and faster code, but no new functionality
 
 		Special thanks to amg0 and akbooer for their support and advise
 		Acknowledgements to akbooer for developing the openLuup environement
@@ -33,66 +32,72 @@ local http = require("socket.http")
 local json = require("dkjson")
 
 local this_device
-
-local WU = {}
-WU["ProviderKey"] = "Enter your WU API key there"
-WU["Location"] = "Enter your WU pws station or location here"
-WU["Period"] = 1800	-- data refresh interval in seconds
-WU["Metric"] = 1	-- 1 = metric units, 0 = US/Imperial, not yet used
-WU["ProviderName"] = "WUI (Weather Underground)" -- added for reference to data source
-WU["ProviderURL"] = "http://www.wunderground.com"
-
-local Str_Obs = "current_observation." -- the "root" key of the useful part of the data table from WU
-local Str_Temperature = "temp_c" 	-- the name of the raw variable name for the device temp
-									-- automatically changed in init() to "temp_f" for Farenheit based on user_data attribute
-local Str_Humidity = "relative_humidity"  -- the name of the raw variable name for the device humidity
-local Str_LastUpdate = "observation_epoch" -- the unix timestamp of the last WU observation
-local Str_Condition = "weather" -- the current conditions reported
-local Str_WindCondition = "wind_string" -- the wind string (to do : localize units MPH v.s. KPH)
-local Str_IconURL = "icon_url" -- the url pointing to the current weather ico (to do : display in UI device interface)
-local Str_ConditionGroup = "icon" -- the condition to be used to fetch the icon and display it with AltUI
-
-local SID_Weather = "urn:upnp-micasaverde-com:serviceId:Weather1"
-
--- the following table lists all variables we want to be reported in the plugin device
-local dvars = {}
-dvars["Temperature"] = {"urn:upnp-org:serviceId:TemperatureSensor1", "CurrentTemperature", "0"}
-dvars["Humidity"] = {"urn:micasaverde-com:serviceId:HumiditySensor1", "CurrentLevel", "0"}
-dvars["LastUpdate"] = {SID_Weather, "LastUpdate", "1456988176"}
-dvars["Condition"] = {SID_Weather, "Condition", ""}
-dvars["ConditionGroup"] = {SID_Weather, "ConditionGroup", ""}
-dvars["WindCondition"] = {SID_Weather, "WindCondition", ""}
-dvars["IconUrl"] = {SID_Weather, "IconUrl", ""}
-
--- children devices
 local child_temperature
 local child_humidity
 
+local SID_Weather = "urn:upnp-micasaverde-com:serviceId:Weather1"
+local WU_urltemplate = "http://api.wunderground.com/api/%s/%s/q/%s.json"
+
+
+local WU = {
+	ProviderKey = "Enter your WU API key there",
+	Location = "Enter your WU pws station or location here",
+	Period = 1800,	-- data refresh interval in seconds
+	Metric = 1,	-- 1 = metric units, 0 = US/Imperial, not yet used
+	ProviderName = "WUI (Weather Underground)", -- added for reference to data source
+	ProviderURL = "http://www.wunderground.com"
+	}
+
+local VariablesMap = {
+	current_observation_temp_c = {serviceId = "urn:upnp-org:serviceId:TemperatureSensor1", variable = "CurrentTemperature"},
+	current_observation_temp_f = {serviceId = "urn:upnp-org:serviceId:TemperatureSensor1", variable = "CurrentTemperature"},
+	current_observation_relative_humidity = {serviceId = "urn:micasaverde-com:serviceId:HumiditySensor1", variable = "CurrentLevel", pattern = "%%"},
+	current_observation_observation_epoch = {serviceId = SID_Weather, variable = "LastUpdate"},
+	current_observation_weather = {serviceId = SID_Weather, variable = "Condition"},
+	current_observation_wind_string = {serviceId = SID_Weather, variable = "WindCondition"},
+	current_observation_icon_url = {serviceId = SID_Weather, variable = "IconUrl"},
+	current_observation_icon = {serviceId = SID_Weather, variable = "ConditionGroup"}
+	}
+
 -- functions
 
-function setvariables(key, value) -- extract the variables we want for our device from the WU data
-	if key == Str_Obs .. Str_Temperature then dvars["Temperature"][3] = value
-	elseif key == Str_Obs .. Str_Humidity then dvars["Humidity"][3] = string.gsub(value, "%%", "") -- !!! need to trim the "%" from the WU data
-	elseif key == Str_Obs .. Str_LastUpdate then dvars["LastUpdate"][3] = value
-	elseif key == Str_Obs .. Str_Condition then dvars["Condition"][3] = value
-	elseif key == Str_Obs .. Str_ConditionGroup then dvars["ConditionGroup"][3] = value
-	elseif key == Str_Obs .. Str_WindCondition then dvars["WindCondition"][3] = value
-	elseif key == Str_Obs .. Str_IconURL then dvars["IconUrl"][3] = value
---	uncomment line below if all data from WU are desired as device variables (50+ !!!)
---	else dvars[key] = {SID_Weather, key, value}
+local function nicelog(message)
+	local display = "openWeather : %s"
+	message = message or ""
+	if type(message) == "table" then message = table.concat(message) end
+	luup.log(string.format(display, message))
+--	print(string.format(display, message))
+end
+
+local function setVar (service, name, value, device) -- credit to @akbooer
+  device = device or this_device
+  local old = luup.variable_get (service, name, device)
+  if tostring(value) ~= old then
+   luup.variable_set (service, name, value, device)
+  end
+end
+
+function setvariables(key, value) -- process the WU data as needed
+	if VariablesMap[key] then
+		if VariablesMap[key].pattern then value = string.gsub(value, VariablesMap[key].pattern, "") end
+		setVar(VariablesMap[key].serviceId, VariablesMap[key].variable, value)
+		if VariablesMap[key].serviceId == "urn:upnp-org:serviceId:TemperatureSensor1" then -- we update the child device as well
+			setVar(VariablesMap[key].serviceId, VariablesMap[key].variable, value, child_temperature)
+		end
+		if VariablesMap[key].serviceId == "urn:micasaverde-com:serviceId:HumiditySensor1" then -- we update the child device as well
+			setVar(VariablesMap[key].serviceId, VariablesMap[key].variable, value, child_humidity)
+		end
+--		nicelog({VariablesMap[key].serviceId," - ", VariablesMap[key].variable, " = ", value})
 	end
 end
 
 function extractloop(datatable, keystring)
-	local tempstr
+	local tempstr, separator
 	keystring = keystring or ""
 	for tkey, value in pairs(datatable) do
-		if keystring == "" then
-			tempstr = tkey
-		else
-			tempstr = keystring .. "." .. tkey
-		end
-		if type(value) == "table" then
+		if keystring ~= "" then separator = "_" else separator = "" end
+		tempstr = table.concat{keystring, separator, tkey}
+		if type(value) == "table" then -- one level up in the data hierarchy -> recursive call
 			extractloop(value, tempstr)
 		else
 			setvariables(tempstr, value)
@@ -101,15 +106,15 @@ function extractloop(datatable, keystring)
 end
 
 function WU_GetData(category) -- call the WU API with our key and location parameters and decode/parse the weather data
--- get current conditions
-	local url = "http://api.wunderground.com/api/" .. WU["ProviderKey"] .. "/" ..category .. "/q/" .. WU["Location"] .. ".json"
+	local url = string.format(WU_urltemplate, WU.ProviderKey, category, WU.Location)
 	local wdata, retcode = http.request(url)
 	local err = (retcode ~=200)
 	if err then -- something wrong happpened (website down, wrong key or location)
 		wdata = nil -- to do: proper error handling
+		nicelog({"WU call failed with http code =  ", tostring(retcode)})
 	else
 		wdata, err = json.decode(wdata)
-		if not (err == 225) then extractloop(wdata) end
+		if not (err == 225) then extractloop(wdata) else nicelog({"WU json decode error = ", tostring(err)}) end
 	end
 	return err
 end
@@ -125,16 +130,7 @@ end
 
 function Weather_delay_callback() -- poll Weather Undergound for changes
 	check_param_updates()
-	local nodata = WU_GetData("conditions")
-	for key, value in pairs(dvars) do
-		if luup.variable_get(value[1], value[2], this_device) ~= value[3] then -- only update if there is a change... better for watches
-			luup.variable_set(value[1], value[2], value[3], this_device)
-			if key == "Temperature" then
-				luup.variable_set(value[1], value[2], value[3], child_temperature) -- update child temperature
-			elseif key == "Humidity" then
-				luup.variable_set(value[1], value[2], value[3], child_humidity) end -- update child humidity
-		end
-	end
+	WU_GetData("conditions") -- get current conditions (function WU_GetData is already designed to get other weather data in future versions)
 	luup.call_delay ("Weather_delay_callback", WU["Period"])
 end
 
@@ -157,8 +153,14 @@ end
 
 function init(lul_device)
 	this_device = lul_device
-	if luup.attr_get("TemperatureFormat") == "F" then Str_Temperature = "temp_f" end -- localize for Farenheit
+	nicelog("device startup")
+	if luup.attr_get("TemperatureFormat") == "F" then -- localize for Farenheit or Celsius based on openLuup setup
+		VariablesMap.current_observation_temp_c = nil
+	else
+		VariablesMap.current_observation_temp_f = nil
+	end
 	createchildren(this_device)
 	Weather_delay_callback()
+	nicelog("device started")
 	return true, "OK", _NAME
 end
